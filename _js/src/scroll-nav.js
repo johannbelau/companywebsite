ad7286badd8cf3cde6d09eaf532da09535f892c3
Andrii S <andriisharapatov@gmail.com>
2020-11-19 17:36:55 +0200

    add jekyll layout includes data pages

---
+++ _js/src/scroll-nav.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function () {
+  const navbar = document.querySelector("nav.navbar");
+  const navbarHeight = navbar.offsetHeight;
+
+  function checkPosition() {
+    const winnY = window.scrollY;
+    const hasBgClass = navbar.classList.contains("bg-white");
+    const scrolledThrough = winnY >= navbarHeight;
+    if (scrolledThrough && !hasBgClass) {
+      document.body.classList.add("nav-bg-white");
+    } else {
+      document.body.classList.remove("nav-bg-white");
+    }
+  }
+
+  window.addEventListener("scroll", checkPosition);
+});
