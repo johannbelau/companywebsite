501da005fce700b9e88297f0653910dccf7d24f0
Andrii S <andriisharapatov@gmail.com>
2020-12-11 16:57:23 +0200

    feature section

--- src/components/services-icons.js
+++ src/components/services-icons.js
@@ -1,28 +1,66 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
 export default function () {
   return (
-    <section className="py-5">
-      <div className="container">
-        <div className="row">
-          
-          <div className="col-lg-4 mt-5 mt-lg-0">
-            <div className="card border-0">
-              <div className="card-body text-center">
-                <img src="/img/sync-alt-solid.svg" className="w-50 mx-auto mb-3" />
-                <p className="h3 font-weight-bold font-alt mb-3">Continuous integration</p>
-                {/* <p className="lead">
-                  Lorem ipsum dolor sit amet consectetur adipisicing.
-                </p> */}
-                <p>
-                  <a href="" className="btn btn-primary btn-rounded-lg ">Learn more</a>
-                </p>
+
+    <StaticQuery 
+    
+      query={graphql`
+        query MyQuery {
+          allContentfulFeature(filter: {parent: {}, node_locale: {eq: "en"}}, sort: {order: ASC, fields: createdAt}) {
+            edges {
+              node {
+                ctaText
+                createdAt
+                image {
+                  file {
+                    url
+                  }
+                }
+                title
+              }
+            }
+          }
+        }
+
+      `}
+
+      render={data => {
+        const features = data.allContentfulFeature.edges
+        return (
+          (
+            <section className="py-5">
+              <div className="container">
+                <div className="row">
+                  { features.map((i,idx) => {
+                    return (
+                      <div className="col-lg-4 col-md-4 col-sm-6 mt-5 mt-lg-0">
+                        <div className="card border-0">
+                          <div className="card-body text-center">
+                            <img src={i.node.image.file.url} 
+                              className="w-50 mx-auto mb-3" 
+                              style={{maxWidth: '120px'}} />
+                            <p className="h5 font-weight-bold font-alt mb-3">{i.node.title}</p>
+                            {/* <p className="lead">
+                              Lorem ipsum dolor sit amet consectetur adipisicing.
+                            </p> */}
+                            <p>
+                              <a href="" className="btn btn-primary btn-rounded-lg ">{i.node.ctaText}</a>
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    )
+                  })}
+                </div>
               </div>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-    </section>
+            </section>
+          )
+        )
+      }}
+    />
+
+    
   )
 }
