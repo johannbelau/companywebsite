a2462affb79494c4d5597277c8809e8377636653
Andrii S <andriisharapatov@gmail.com>
2020-12-18 11:27:44 +0200

    our company responsive fix

--- src/components/our-company.js
+++ src/components/our-company.js
@@ -39,19 +39,19 @@ export default function (props) {
       <section className="bg-light">
         <div className="container">
           <div className="row pt-5">
-            <div className="col-lg-3">
+            <div className="col-xl-3 col-lg-4 col-md-auto">
               <div className="bg-primary shadow-lg rounded-lg d-flex h-100 p-4">
                 <h2 className="text-white h1 font-weight-normal align-self-center font-alt">
                   {(lang === 'de') ? 'Unser Unternehmen' : 'Our Company'}
                 </h2>
               </div>
             </div>
-            <div className="col-lg-9">
+            <div className="col-xl-9 col-lg-8 col-md-12">
               <div className="p-4">
                 <div className="row">
                   { filtered.map((i,idx) => {
                     return (
-                      <div className="col-lg-6" key={`service-${idx}`}>
+                      <div className="col-lg-6 col-md-6" key={`service-${idx}`}>
                         <h3 className="mt-4 font-weight-bold font-alt">{i.node.heading}</h3>
                         <p>
                           {i.node.description.description}
